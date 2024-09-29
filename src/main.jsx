fe01a8d819a9a5da0bffa60add551adb58c216df
askuenik <askue.nik@gmail.com>
2024-09-30 02:49:13 +0300

    Ваше сообщение о коммите

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
+import  WebApp  from '@twa-dev/sdk';
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
 
-
+WebApp.ready();
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
